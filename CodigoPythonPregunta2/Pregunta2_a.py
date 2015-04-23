0492bf0e9716074af64f84b4ce4bcbadde5d8a94
psigelo <pascal.sigel@gmail.com>
2015-04-22 23:42:29 -0300

    Se hace el primer push

--- CodigoPythonPregunta2/Pregunta2_a.py
+++ CodigoPythonPregunta2/Pregunta2_a.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -42,39 +41,3 @@ if __name__ == "__main__":
 	plt.plot(datos_betta_teorema_lim_central, color='red')
 	plt.plot(esperanza_teorica_beta, color='blue')
 	plt.show()
-
-
-	# """
-	# Demo of the histogram (hist) function with a few features.
-
-	# In addition to the basic histogram, this demo shows a few optional features:
-
-	#     * Setting the number of data bins
-	#     * The ``normed`` flag, which normalizes bin heights so that the integral of
-	#       the histogram is 1. The resulting histogram is a probability density.
-	#     * Setting the face color of the bars
-	#     * Setting the opacity (alpha value).
-
-	# """
-	
-
-
-	# # example data
-	# mu = 100 # mean of distribution
-	# sigma = 15 # standard deviation of distribution
-	# x = mu + sigma * np.random.randn(10000)
-
-	# num_bins = 50
-	# # the histogram of the data
-	# n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
-	# # add a 'best fit' line
-	# y = mlab.normpdf(bins, mu, sigma)
-	# plt.plot(bins, y, 'r--')
-	# plt.xlabel('Smarts')
-	# plt.ylabel('Probability')
-	# plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
-
-	# # Tweak spacing to prevent clipping of ylabel
-	# plt.subplots_adjust(left=0.15)
-	# plt.show()
-
